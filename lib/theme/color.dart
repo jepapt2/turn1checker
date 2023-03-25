f3af20e3fab09a067d46a2767ec6fe6d2b288cf5
jepapt2 <j.webprogram@gmail.com>
2023-03-25 22:58:33 +0900

    colorTheme

---
+++ lib/theme/color.dart
@@ -0,0 +1,12 @@
+//カラーテーマを定義するクラス
+import 'package:flutter/material.dart';
+
+class ColorTheme {
+  static const Color primary = Color(0xFF42627C);
+  static const Color secondary = Color(0xFF30FAC8);
+  static const Color tertiary = Color(0xFFF3B166);
+  static const Color quaternary = Color(0xFFDFDF93);
+  static const Color background = Color(0xFF233544);
+  static const Color white = Color(0xFFF1F1F1);
+  static const Color black = Color(0xFF1C1C1C);
+}
