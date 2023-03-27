a82660caafb94fa00283f5a097e0c45405a13f6c
jepapt2 <j.webprogram@gmail.com>
2023-03-27 20:40:12 +0900

    Localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:turn1checker/theme/theme.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'router/router.dart';
 
@@ -17,6 +18,7 @@ class App extends StatelessWidget {
     return MaterialApp.router(
       title: 'turn1Checker',
       localizationsDelegates: const [
+        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,10 +8,10 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: Text(AppLocalizations.of(context)!.test),
       ),
       body: Center(
-        child: Text('Home Screen'),
+        child: Text(''),
       ),
     );
   }
