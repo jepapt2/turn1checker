b6ec03acc90b1da6f75e33512730ba044e7eb135
jepapt2 <j.webprogram@gmail.com>
2023-07-11 20:52:19 +0900

    colortheme orangeGradient

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 class ColorTheme {
   static const Color primary = Color(0xFF42627C);
   static const Color secondary = Color(0xFF30FAC8);
-  static const Color tertiary = Color(0xFFF3B166);
-  static const Color quaternary = Color(0xFFDFDF93);
+  static const Color orange = Color(0xFFF3B166);
+  static const Color yellow = Color(0xFFDFDF93);
   static const Color background = Color(0xFF233544);
   static const Color white = Color(0xFFF1F1F1);
   static const Color lightGray = Color(0xFFBDBDBD);
@@ -14,4 +14,6 @@ class ColorTheme {
   static const Color primaryGradientEnd = Color.fromARGB(255, 81, 97, 139);
   static const Color secondaryGradientStart = Color(0xFF4F7697);
   static const Color secondaryGradientEnd = Color.fromARGB(0, 59, 91, 117);
+  static const Color orangeGradientStart = Color(0xFFFEA33B);
+  static const Color orangeGradientEnd = Color(0xFFEEC534);
 }
